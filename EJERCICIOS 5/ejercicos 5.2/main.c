a6ad146ea82e3e2662d91046393f4a694eb75c01
Deuryreyes <Deurirosa92@gmail.com>
2024-06-11 12:04:49 -0400

    Add files via upload

---
+++ EJERCICIOS 5/ejercicos 5.2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int ELE[5] = {0};
+    int I, VOT;
+
+    printf("Ingresa el primer voto (0 - Para terminar): ");
+    scanf("%d", &VOT);
+
+    while (VOT)
+    {
+        if (VOT >= 1 && VOT <= 5)
+            ELE[VOT - 1]++;
+        else
+            printf("\nEl voto ingresado es incorrecto.\n");
+
+        printf("Ingresa el siguiente voto (0 - Para terminar): ");
+        scanf("%d", &VOT);
+    }
+
+    printf("\n\nResultados de la Elección\n");
+
+    for (I = 0; I < 5; I++)
+        printf("\nCandidato %d: %d votos", I + 1, ELE[I]);
+
+    printf("\n");
+
+    return 0;
+}
+
